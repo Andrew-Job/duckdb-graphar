fb138b593fd83a79e547ccc9b4d49e403ebfa2ca
RoyalMirMak <miron.makhlin@gmail.com>
2025-11-24 12:41:27 +0300

    fix style

--- src/utils/global_log_manager.cpp
+++ src/utils/global_log_manager.cpp
@@ -6,7 +6,7 @@ std::optional<std::reference_wrapper<LogManager>> GlobalLogManager::log_manager 
 
 void GlobalLogManager::Initialize(DatabaseInstance& db, LogLevel log_level) {
     log_manager = db.GetLogManager();
-    LogManager &log_manager_ref = log_manager->get();
+    LogManager& log_manager_ref = log_manager->get();
     log_manager_ref.SetEnableLogging(true);
     log_manager_ref.SetLogStorage(db, "stdout");
     log_manager_ref.SetLogLevel(log_level);
